bff25969c0522dff5820bdd0137bbea359a31322
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2020-02-12 20:44:03 +0800

    Add files via upload

---
+++ problems/sum_n/solution.py
@@ -0,0 +1,8 @@
+# Sum N
+
+n = int(input())
+ans = 0
+for i in range(n):
+    ans += int(input())
+
+print(ans)
---
+++ problems/sum_n/test_generator.py
@@ -0,0 +1,6 @@
+from random import randint
+
+n = randint(0, 10000)
+print(n)
+for i in range(n):
+    print(randint(0, 10000))
